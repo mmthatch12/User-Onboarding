c015cd1e1066ee75fe876ed772d67082c3fe40a4
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-07 13:32:21 -0600

    started Form.js

--- useronboarding/src/App.js
+++ useronboarding/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './component/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>User Onboarding</h1>
+      <Form />
     </div>
   );
 }
