06f3bfb77087d488dfc012c5bec1ffabd932f428
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-07 16:25:10 -0600

    added a bit more styling

--- useronboarding/src/component/Form.js
+++ useronboarding/src/component/Form.js
@@ -38,6 +38,13 @@ const UserForm = ({ errors, touched, values, status }) => {
                         <p>{errors.password}</p>
                     )}
 
+                    <Field component="select" name="color">
+                        <option>What is your Favorite Color?</option>
+                        <option className='blue' value="blue">Blue</option>
+                        <option className='red' value="red">Red</option>
+                        <option className='green' value="green">Green</option>
+                    </Field>
+
                     <label>
                         Terms of Service
                         <Field className="formfield" type="checkbox" name="tos" checked={values.tos} />
@@ -58,19 +65,21 @@ const UserForm = ({ errors, touched, values, status }) => {
 }
 
 const FormicUserForm = withFormik({ 
-    mapPropsToValues({ name, email, password, tos }) {
+    mapPropsToValues({ name, email, password, tos,color }) {
         return {
             name: name || "",
             email: email || "",
             password: password || "",
-            tos: tos || false 
+            tos: tos || false,
+            color: color || '' 
         }
     },
 
     validationSchema: Yup.object().shape({
         name: Yup.string().required('Name field is required'),
         email: Yup.string().email('Email not valid').required('Email entry is required'),
-        password: Yup.string().min(7, "Password must be 7 characters or longer").required('Password is required')
+        password: Yup.string().min(7, "Password must be 7 characters or longer").required('Password is required'),
+        color: Yup.string().required('Selection of color is required')
 
     }),
 
--- useronboarding/src/component/UserCard.js
+++ useronboarding/src/component/UserCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const UserCard = ({ userdata }) => {
     return (
-        <div className="userCardDiv">
+        <div>
             {userdata.map(user => (
-                <div key={user.id}>
+                <div key={user.id} className="userCardDiv">
                     <h3>Name: {user.name}</h3>
                     <h3>Email: {user.email}</h3>
+                    <h3>Favorite Color: {user.color}</h3>
                     <h3>Signed Terms of Use: {user.tos.toString() === 'true' ? "Yes" : "Not yet signed"}</h3>
                 </div>
             ))}
